77e14e94d3f84b05c19f953bc0853d6fec05cef7
“terry19970422” <terry19970422@outlook.com>
2022-07-20 10:53:25 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import React, { useEffect } from 'react'
-import {AB_GET_LIST} from './config/ajax-path'
+import React, { useEffect, useState } from "react";
+import { AB_GET_LIST } from "./config/ajax-path";
 
-//componentDidMount
-//componentDidUpdated
-//componentWillMount
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
 
+export default function App() {
+    const [data, setData] = useState({});
 
+    const getData = async () => {
+        const r = await fetch(AB_GET_LIST);
+        const obj = await r.json();
+        console.log(obj);
+        setData(obj);
+    };
 
-function App() {
+    useEffect(() => {
+        getData();
+    }, []);
 
-  const getData = async()=>{
-    const r = await fetch(AB_GET_LIST);
-    const obj = await r.json();
-    console.log(obj);
-  }
-
-  useEffect(()=>{
-    getData();
-  },[])
-
-  return (
-    <div>App</div>
-  )
+    return (
+        <div>
+            {console.log({ data })}
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">姓名</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">手機號碼</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
-
-
-export default App
-
